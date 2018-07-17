91997dfe29cb48a68c5661ba06ce3254be7dc90b
philliphdev <phannafo@us.ibm.com>
2018-07-17 12:19:15 -0400

    Updated logic for database, created seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_16_201812) do
+ActiveRecord::Schema.define(version: 2018_07_17_150119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,7 +46,6 @@ ActiveRecord::Schema.define(version: 2018_07_16_201812) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "cities", "users"
   add_foreign_key "reviews", "cities"
   add_foreign_key "reviews", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+
+City.destroy_all
+
+atlanta = City.new
+atlanta.name = "Atlanta"
+atlanta.image = "http://www.bazaardaily.co.uk/wp-content/uploads/2017/09/Atlanta_Flip_or_Flop.jpg"
+atlanta.save
+
+chicago = City.new
+chicago.name = "Chicago"
+chicago.image = "http://www.vivecampus.com/_assets/uichicago/_img/university-of-illinois-chicago-english-program-schema.jpg"
+chicago.save
+
+newyork = City.new
+newyork.name = "New York"
+newyork.image = "https://www.engineerbabu.com/blog/wp-content/uploads/2015/10/newyork-1.jpg"
+newyork.save
+
+sanfran = City.new
+sanfran.name = "San Francisco"
+sanfran.image ="https://amp.businessinsider.com/images/55f6e585bd86ef20008ba163-1920-960.jpg"
+sanfran.save
+
