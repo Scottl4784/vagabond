fb7bb6a48f589bc2474a6cf17c8bb1b864c4d046
Scottl4784 <Scottl4784@gmail.com>
2018-07-18 16:27:19 -0400

    moved styling to cities componoent

--- client/src/components/NewReviewForm.js
+++ client/src/components/NewReviewForm.js
@@ -2,9 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios'
 
 class NewReviewForm extends Component {
-    state = {
-        title: []
-    }
+   
     handleChange = (event) => {
         const inputName = event.target.name
         const userInput = event.target.value
